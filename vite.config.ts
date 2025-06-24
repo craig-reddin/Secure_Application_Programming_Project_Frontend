0e560d54e352ef57439b395e11428c4ceccba243
Craig Reddin <craig-reddin@hotmail.com>
2025-06-24 19:38:47 +0100

    deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 //https://stackoverflow.com/questions/69417788/vite-https-on-localhost
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync("./privatekey.pem"),
+      cert: fs.readFileSync("./server.crt"),
+    },
     port: 5173,
   },
 });
